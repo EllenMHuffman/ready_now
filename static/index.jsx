98f0bca94e3520600db545374e3cfe3f8b745d37
Ellen Huffman <ellenmhuffman@gmail.com>
2018-02-13 01:10:24 +0000

    create react components for ActivitiesContainer and Activity

--- static/index.jsx
+++ static/index.jsx
@@ -1,6 +1,15 @@
-'use strict';
+// // 'use strict';
 
 ReactDOM.render(
-    <Timer name=act_name time=act_time />,
+    <ActivitiesContainer data={ activity_time } />,
     document.getElementById('root')
 )
+
+//         let items = [];
+//         for (let activity of this.props.data) {
+//             items.push(<div class='act_name'>Activity Name: { activity[this.props.data] },
+//             <span class='act_time'>Activity Time: { activity }</span> </div>)
+//         };
+//         return (
+//             items
+//         );
--- static/timer.jsx
+++ static/timer.jsx
@@ -1,7 +1,37 @@
 'use strict';
 
-class Activities extends React.Component {
+class ActivitiesContainer extends React.Component {
+    startActivities() {
+        alert('Working!!')
+    }
+
     render() {
-        return <div>Activity Name: { this.props.name }, Activity Time: { this.props.time }</div>
+        let activities = [];
+        // debugger;
+        for (let activity in this.props.data) {
+            let current_activity = this.props.data[activity];
+            activities.push(<Activity id={activity} name={current_activity[0]}
+                            time={current_activity[1]} />)
+        }
+        return <div>
+        <button onClick={this.startActivities} type='button' id='start'>Start</button>
+        {activities}
+        <div>ETA</div>
+        </div>;
+    }
+
+
+    // timer(time) {
+    //     interval = setInterval(function () {
+    //     { this.time };
+    //     time -= 1;
+    //     }, 1000);
+    // }
+}
+
+class Activity extends React.Component {
+    render() {
+        debugger;
+        return <div><span> {this.props.name}</span> <span> {this.props.time}</span></div>
     }
 }
