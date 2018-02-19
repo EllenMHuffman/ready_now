c9dc04e849a3c46575f03c06a303fd1ec0012c2e
Ellen Huffman <ellenmhuffman@gmail.com>
2018-02-19 03:15:51 +0000

    render homepage entirely in react

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -17,39 +17,52 @@ class ActivitiesContainer extends React.Component {
       .then((data)=>  this.setState({data}));
   }
 
-  handleClick() {
-    let nextState = this.state;
+  calculateTime() {
+    let totalTime = 0;
+    for (let act_id in this.state.data) {
+      if (this.state.data[act_id]['clicked'] === true) {
+        totalTime += this.state.data[act_id]['time'];
+      }
+    }
+    return (totalTime/60).toFixed(2);
+  }
+
+  handleClick(event) {
+    event.preventDefault();
+    let act_id = event.target.value;
+    let nextState = this.state.data;
     nextState[act_id]['clicked'] = !(nextState[act_id]['clicked']);
     this.setState(nextState);
   }
 
-  handleSubmit() {
-    // HANDLE SUBMIT, PREVENT DEFAULT
-    console.log('handleSubmit loaded');
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log('handleSubmit');
   }
 
   render() {
     let activities = [];
+    console.log(this.state.data);
     for (let act_id in this.state.data) {
       activities.push(<ActivityForm key={act_id}
+                                    act_id={act_id}
                                     time={this.state.data[act_id]['time']}
                                     name={this.state.data[act_id]['name']}
-                                    handleClick={this.handleClick}
-                                    handleSubmit={this.handleSubmit} />);
+                                    handleClick={this.handleClick} />);
     }
     return (
       <div>
         <h1>Welcome to Ready Now!</h1>
         <br />
         <h2>What do you need to do?</h2>
-        <form>
+        <div>
           {activities}
-          <div>
-            <b>Total time:</b>
-            <span id='UPDATEWITHCLASS'></span>
-          </div>
-          <button type='submit' onSubmit={this.props.handleSubmit}>Go!</button>
-        </form>
+            <br />
+            <b>Total time: </b>
+            <span id='UPDATEWITHCLASS'>{this.calculateTime()}</span>
+            <br />
+          <button id='submit' onClick={this.handleSubmit}>Go!</button>
+        </div>
       </div>
     );
   }
@@ -59,7 +72,7 @@ class ActivityForm extends React.Component {
   render() {
     return (
       <div>
-        <button onClick={this.props.handleClick}>{this.props.name}:
+        <button value={this.props.act_id} onClick={this.props.handleClick}>{this.props.name}:
                          ~{this.props.time/60} mins</button>
       </div>
     );
